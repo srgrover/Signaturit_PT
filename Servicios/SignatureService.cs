e3c48dd410f6064362e19337af8788b333e63721
Jonathan <djgrovezone@gmail.com>
2022-07-05 20:01:35 +0200

    Agregamos nuevo metodo para obtener la firma necesaria para ganar. Metodos SignatureToWinByContract y IsContractIncomplete nuevos. Modificado GetPuntosByContract

--- Servicios/SignatureService.cs
+++ Servicios/SignatureService.cs
@@ -1,44 +1,77 @@
-﻿using Signaturit_PT.Domain.Entities;
-using Signaturit_PT.Domain.Interfaces;
+﻿using Signaturit_PT.Entities;
+using Signaturit_PT.Interfaces;
 
 namespace Signaturit_PT.Servicios
 {
     public class SignatureService : ISignature
     {
-        string ISignature.WinnerByContract(ContractVersus contracts)
+        enum valorFirma { V = 1, N = 2, K = 5};
+
+        public string WinnerByContract(ContractVersus contracts)
         {
-            int puntosA = GetPuntosByContract(contracts.contractA.ToUpper());
-            int puntosB = GetPuntosByContract(contracts.contractB.ToUpper());
+            int puntosA = GetPuntosByContract((contracts.contractA ?? "").ToUpper());
+            int puntosB = GetPuntosByContract((contracts.contractB ?? "").ToUpper());
 
             return (puntosA > puntosB) ? "Contract A Gana" : (puntosA == puntosB) ? "Empate" : "Contract B Gana";
+        }
+
+        public string SignatureToWinByContract(ContractVersus contracts)
+        {
+            bool contrAIncomplete = IsContractIncomplete(contracts.contractA ?? "");
+            bool contrBIncomplete = IsContractIncomplete(contracts.contractB ?? "");
+
+            if (contrAIncomplete && contrBIncomplete || !contrAIncomplete && !contrBIncomplete)
+            {
+                return "Ingrese almenos un contrato completo";
+            } 
+            else
+            {
+                int puntosA = GetPuntosByContract((contracts.contractA ?? "").ToUpper());
+                int puntosB = GetPuntosByContract((contracts.contractB ?? "").ToUpper());
+
+                if (contrAIncomplete) return "Contrato A " + GetFirmaGanadoraByContracts(puntosB, puntosA);
+                else return "Contrato B " + GetFirmaGanadoraByContracts(puntosA, puntosB);
+            }
+        }
+
+        private string GetFirmaGanadoraByContracts(int puntosContratoCompleto, int puntosContratoIncompleto)
+        {
+            foreach (int puntosExtra in Enum.GetValues(typeof(valorFirma)))
+            {
+                if ((puntosContratoIncompleto + puntosExtra) > puntosContratoCompleto)
+                {
+                    return "necesita la firma: " + Enum.GetName(typeof(valorFirma), puntosExtra) ?? "";
+                }
+            }
+
+            return "No hay firma ganadora";
         }
 
         private int GetPuntosByContract(string contract)
         {
             int puntuacion = 0;
 
-            if (string.IsNullOrEmpty(contract))
-            {
-                return 0;
-            }
-
             foreach (var i in contract)
             {
                 switch (i)
                 {
                     case 'K':
-                        puntuacion += 5;
+                        puntuacion += (int)valorFirma.K;
                         break;
                     case 'N':
-                        puntuacion += 2;
+                        puntuacion += (int)valorFirma.N;
                         break;
                     case 'V':
-                        puntuacion += 1;
+                        if(contract.IndexOf("K") == -1) puntuacion += (int)valorFirma.V;
                         break;
                 }
             }
 
             return puntuacion;
+        }
+
+        private bool IsContractIncomplete(string contract){
+            return contract.IndexOf("#") != -1;
         }
     }
 }
